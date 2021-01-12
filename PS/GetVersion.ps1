e076f348f9ba5e9e2613e9716f9d7b2429ad4e94
Jimmi Lyhne Andersen <ja@inmento.dk>
2021-01-12 09:19:13 +0100

    NC-2884 Creating release notes 1

--- PS/GetVersion.ps1
+++ PS/GetVersion.ps1
@@ -1,2 +1,16 @@
-$versionFromServer = (Invoke-webrequest -URI "https://www.3shape.com/version.txt").Content
-Write-Host "Currently deployed build: " $versionFromServer.Trim()
+try{
+
+    $versionFromServer = (Invoke-webrequest -URI "https://www.3shape.com/currentbuildtag.txt")
+    # $versionFromServer = (Invoke-webrequest -URI "https://3shape-sitecore-test1-cd.azurewebsites.net/currentbuildtag.txt").Content
+    Write-Host "Status code" $versionFromServer.StatusCode
+    if($versionFromServer.StatusCode -eq 200){
+        Write-Host "Currently deployed build: "$versionFromServer.Content
+    }
+    
+}
+catch{
+    Write-Host "Could not access file" $_.Exception.Response.StatusCode.value__
+    exit 0
+    Write-Host "Should not be here"
+}
+Write-Host "After try catch"$versionFromServer
